b0fe3946bb3da8fe9bdd73206da083a88f81bdf9
Tiziano D'Albis <tdalbis@univ-rennes1.fr>
2017-03-16 19:53:56 +0100

    initial push neural fields with pyopencl

---
+++ neural_field/kernel.cl
@@ -0,0 +1,83 @@
+
+
+float sq(float x){
+ return x*x;
+}
+
+float w(float d)
+{
+  const float A=5.;
+  const float a=.125;
+
+  const float B=4.;
+  const float b=.005;
+
+  //const float p=1.;
+
+  //return A*pow(a,p/(float)2.)*exp(-a*pow(d,(float)2.))-B*pow(b,p/(float)2.)*exp(-b*pow(d,(float)2.));
+
+  return A*pow(a,(float)0.5)*exp(-a*pow(d,(float)2.))-B*pow(b,(float)0.5)*exp(-b*pow(d,(float)2.));
+}
+
+
+float dist(float x, float y)
+{
+ return x-y;
+}
+float F(float u)
+{
+  const float u_th=0.05;
+  const float r=0.25;
+  const float k=.24;
+
+
+  float df0=r/4.;
+  float f= 1./(1.+exp(-r*(u-u_th)));
+  float f0 = 1./(1.+exp(r*u_th));
+
+  return k*(f-f0)/df0; 
+
+}
+
+__kernel void mytest(
+    __global const float *x_g,
+    __global float *res_x_g
+)
+{
+  int gid = get_global_id(0);
+  res_x_g[gid]=w(x_g[gid]);
+}
+
+__kernel void pattern(
+    __global const float *x_g,
+    __global const float *u_g,
+    __global const float *v_g,
+    __global const float *I_g,    
+    __global float *res_u_g,
+    __global float *res_v_g,
+   int nx
+)
+{
+
+  const float alpha=0.1;
+  const float beta=0.;
+  const float tau=1.;
+
+
+  int gid = get_global_id(0);
+  float u =u_g[gid];
+  float v =v_g[gid];
+  float I =I_g[gid];
+
+
+  res_u_g[gid]=-u-beta*v+I;
+  
+  float conv=0.;
+  for(int i=0;i<nx; i++)
+  {
+    conv+=F(u_g[i])*w(dist(x_g[gid],x_g[i]));
+  }   
+  res_u_g[gid]+=(M_PI_F*2/nx)*conv;
+  res_v_g[gid]=alpha*(u-v);
+
+}
---
+++ neural_field/pattern.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pyopencl as cl
+
+ctx = cl.create_some_context()
+queue = cl.CommandQueue(ctx)
+
+nx =1000
+tmax=50
+
+x=np.linspace(-np.pi,np.pi,nx,endpoint=False,dtype=np.float32)
+u=np.ones(nx,dtype=np.float32)
+v=np.zeros(nx,dtype=np.float32)
+I=np.zeros(nx,dtype=np.float32)
+time=np.arange(tmax)
+
+mf = cl.mem_flags
+
+x_g = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=x)
+u_g = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=u)
+v_g = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=v)
+I_g = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=I)
+
+res_u_g = cl.Buffer(ctx, mf.READ_WRITE, u.nbytes)
+res_v_g = cl.Buffer(ctx, mf.READ_WRITE, v.nbytes)
+
+src_file = open('kernel.cl','r') 
+src=src_file.read()
+src_file.close()
+prg = cl.Program(ctx,src).build()
+
+
+u_mat=np.zeros((tmax,nx))
+v_mat=np.zeros((tmax,nx))
+
+for t in xrange(tmax):
+  prg.pattern(queue, (nx,), None, x_g, u_g, v_g, I_g, res_u_g, res_v_g,np.int32(nx))
+
+  cl.enqueue_copy(queue, u, res_u_g)
+  cl.enqueue_copy(queue, v, res_v_g)
+
+  u_mat[t,:]=u
+  v_mat[t,:]=v
+  
+  
+import pylab as pl
+pl.figure()
+pl.pcolormesh(x,time,u_mat)
+pl.colorbar()
+print u_mat.max()
+
+
+res_x_g = cl.Buffer(ctx, mf.READ_WRITE, x.nbytes)
+
+res_x=np.zeros_like(x)
+prg.mytest(queue, (nx,), None, x_g, res_x_g)
+cl.enqueue_copy(queue, res_x, res_x_g)
+
+
+pl.figure()
+pl.plot(x,res_x)
+
+mexfft=abs(np.fft.fft(res_x))
+freqs=np.fft.fftfreq(nx,2*np.pi/nx)
+
+pl.figure()
+pl.plot(freqs,mexfft)
+pl.xlim((-5,5))
