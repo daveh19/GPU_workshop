50596e0001c1bd76ec1c73ae3cc39707b39e8c94
Thomas McColgan <thomas.mccolgan@gmail.com>
2017-03-16 10:42:51 +0100

    example from the github page

---
+++ Julia/example.jl
@@ -0,0 +1,32 @@
+using OpenCL
+
+const sum_kernel = "
+   __kernel void sum(__global const float *a,
+                     __global const float *b,
+                     __global float *c)
+    {
+      int gid = get_global_id(0);
+      c[gid] = a[gid] + b[gid];
+    }
+"
+a = rand(Float32, 50_000)
+b = rand(Float32, 50_000)
+
+device, ctx, queue = cl.create_compute_context()
+
+a_buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=a)
+b_buff = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=b)
+c_buff = cl.Buffer(Float32, ctx, :w, length(a))
+
+p = cl.Program(ctx, source=sum_kernel) |> cl.build!
+k = cl.Kernel(p, "sum")
+
+queue(k, size(a), nothing, a_buff, b_buff, c_buff)
+
+r = cl.read(queue, c_buff)
+
+if isapprox(norm(r - (a+b)), zero(Float32))
+    info("Success!")
+else
+    error("Norm should be 0.0f")
+end
